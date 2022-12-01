70126f76367bbaa9e4f6ab7f40d339cf3c659f08
Tommy Goossens <t.goossens@vgz.nl>
2022-12-01 13:43:58 +0100

    removed commented code

--- AdventOfCode2022/Day1/ProgramDay1.cs
+++ AdventOfCode2022/Day1/ProgramDay1.cs
@@ -7,7 +7,7 @@
 
         protected override string Run()
         {
-            string[] lines = File.ReadAllLines(@"Day1\advent_of_code_1_input_large.txt");
+            string[] lines = File.ReadAllLines(@"Day1\advent_of_code_1_input.txt");
             var caloriesPerElf = new List<int>();
             var temp = 0;
             foreach(string line in lines)
@@ -24,26 +24,7 @@
             orderedElfList = orderedElfList.Take(3);
             var mostCalories = orderedElfList.First(); // Part1
             var top3 = orderedElfList.Sum(e => e); // Part 2
-            return $"Most {mostCalories} calories. The sum of the top 3 elfs is: {top3}";
+            return $"Most calories: {mostCalories}. The sum of the top 3 elfs is: {top3}";
         }
-
-        // OLD
-        //protected override string Run()
-        //{
-        //    string[] lines = File.ReadAllLines(@"Day1\advent_of_code_1_input.txt");
-        //    int currentElf = 1;
-        //    var caloriesPerElf = new Dictionary<int, List<int>>();
-        //    foreach (string line in lines)
-        //    {
-        //        if (string.IsNullOrEmpty(line)) { currentElf++; continue; }
-
-        //        caloriesPerElf.TryAdd(currentElf, new List<int>());
-        //        caloriesPerElf[currentElf].Add(int.Parse(line));
-        //    }
-        //    var orderedElfList = caloriesPerElf.ToDictionary(d => d.Key, d => d.Value.Sum()).OrderByDescending(d => d.Value);
-        //    var elfWithMostCalories = orderedElfList.First(); // Part1
-        //    var top3 = orderedElfList.Take(3).Sum(e => e.Value); // Part 2
-        //    return $"Elf nr {elfWithMostCalories.Key} with {elfWithMostCalories.Value} calories. The sum of the top 3 elfs is: {top3}";
-        //}
     }
 }
