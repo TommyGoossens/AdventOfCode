5010a660df2ea3fd3b0f6d81e9437e82fcf33853
Tommy Goossens <t.goossens@vgz.nl>
2022-12-03 09:54:12 +0100

    Day 3

---
+++ AdventOfCode2021/Day3/ProgramDay3.cs
@@ -0,0 +1,48 @@
+﻿using AdventOfCodeShared;
+using System.Numerics;
+
+namespace AdventOfCode2021.Day3
+{
+    internal static class Helper
+    {
+        internal static string To8BitString(this string original)
+        {
+            var len = original.Length;
+            var missing = 8 - len;
+            for (int i = 0; i < missing; i++) original = "0" + original;
+            return original;
+        }
+
+        internal static string FlipBits(this string original)
+        {
+            var result = "";
+            foreach (var c in original) result += c == '0' ? "1" : "0";
+            return result.To8BitString();
+        }
+
+    }
+    internal class ProgramDay3 : AdventOfCodeProgram
+    {
+        public ProgramDay3() : base(3)
+        {
+        }
+
+        protected override string[] Run()
+        {
+            var binaryNumber = "";
+            for (int i = 0; i < lines.First().Length; i++)
+            {
+                var col = lines.Select(l => l[i]).ToList();
+                var nrOfZeros = col.Count(l => l == '0');
+                var nrOfOnes = col.Count(l => l == '1');
+                if (nrOfZeros > nrOfOnes) binaryNumber += "0";
+                else binaryNumber += "1";
+            }
+
+            var gamma = Convert.ToInt32(binaryNumber.To8BitString(), 2);
+            var epsilon = Convert.ToInt32(binaryNumber.FlipBits(), 2);
+
+            return new string[] { $"Gamma ({gamma}) * Epsilon ({epsilon}) = {gamma * epsilon}" };
+        }
+    }
+}
---
+++ AdventOfCode2022/Day3/ProgramDay3.cs
@@ -0,0 +1,60 @@
+﻿using AdventOfCodeShared;
+using System.Collections.Generic;
+
+namespace AdventOfCode2022.Day3
+{
+    internal class ProgramDay3 : AdventOfCodeProgram
+    {
+        private Dictionary<char, int> letterValues;
+
+        public ProgramDay3() : base(3)
+        {
+            letterValues = new Dictionary<char, int>();
+            var allLetters = Enumerable.Range('a', 26);
+            for (int i = 0; i < allLetters.Count(); i++)
+            {
+                var letter = (char)allLetters.ElementAt(i);
+                letterValues.Add(letter, i + 1);
+                letterValues.Add(Char.ToUpper(letter), i + 27);
+            }
+        }
+
+        protected override string[] Run()
+        {
+            var prioritySum = GetAnswerPart1();
+            var badgesSum = GetAnswerPart2();
+            return new string[] { $"The sum of priorities is: {prioritySum}", $"The sum of badges is: {badgesSum}" };
+        }
+
+        private int GetAnswerPart1()
+        {
+            var total = 0;
+            lines.AsParallel().ForAll(l =>
+            {
+                var firstHalf = l.Substring(0, l.Length / 2);
+                var secondHalf = l.Substring(l.Length / 2);
+                var itemsInBoth = firstHalf.Intersect(secondHalf);
+                Interlocked.Add(ref total, GetValueOfIntersectedChars(itemsInBoth));
+            });
+            return total;
+        }
+
+        private int GetAnswerPart2()
+        {
+            var total = 0;
+            var i = 0;
+            while ((i * 3) != lines.Length)
+            {
+                var batch = lines.Skip(i * 3).Take(3);
+                var intersection = batch.Skip(1).Aggregate(new HashSet<char>(batch.First()),
+                    (h, e) => { h.IntersectWith(e); return h; }).ToList();
+                total += GetValueOfIntersectedChars(intersection);
+                i++;
+            }
+
+            return total;
+        }
+
+        private int GetValueOfIntersectedChars(IEnumerable<char> chars) => chars.Select(c => letterValues[c]).Sum();
+    }
+}
