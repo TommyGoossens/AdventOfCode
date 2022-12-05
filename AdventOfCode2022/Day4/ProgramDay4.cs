570cc1e19414975bbbd1d9b01ff9f15e59a74fbb
Tommy Goossens <t.goossens@vgz.nl>
2022-12-05 12:56:12 +0100

    2022 4 + 5

---
+++ AdventOfCode2022/Day4/ProgramDay4.cs
@@ -0,0 +1,47 @@
+﻿using AdventOfCodeShared;
+
+namespace AdventOfCode2022.Day4
+{
+    internal class ProgramDay4 : AdventOfCodeProgram
+    {
+        public ProgramDay4() : base(4)
+        {
+        }
+
+        protected override string[] Run()
+        {
+            var allContainedRows = lines.Where(IsContained);
+            var allOverlappingRows = lines.Where(HasOverlap);
+            return new string[] {$"Contained pairs: {allContainedRows.Count()}", $"Pairs with overlap {allOverlappingRows.Count()}" };
+        }
+
+        private bool IsContained(string l)
+        {
+            var first = l.Split(',')[0];
+            var second = l.Split(',')[1];
+            var firstStart = int.Parse(first.Split('-')[0]);
+            var firstEnd = int.Parse(first.Split('-')[1]);
+            var secondStart = int.Parse(second.Split('-')[0]);
+            var secondEnd = int.Parse(second.Split('-')[1]);
+
+            var firstRange = Enumerable.Range(firstStart, firstEnd - firstStart + 1);
+            var secondRange = Enumerable.Range(secondStart, secondEnd - secondStart + 1);
+            return firstRange.Intersect(secondRange).Count() == firstRange.Count() || secondRange.Intersect(firstRange).Count() == secondRange.Count();
+        }
+
+
+        private bool HasOverlap(string l)
+        {
+            var first = l.Split(',')[0];
+            var second = l.Split(',')[1];
+            var firstStart = int.Parse(first.Split('-')[0]);
+            var firstEnd = int.Parse(first.Split('-')[1]);
+            var secondStart = int.Parse(second.Split('-')[0]);
+            var secondEnd = int.Parse(second.Split('-')[1]);
+
+            var firstRange = Enumerable.Range(firstStart, firstEnd - firstStart + 1);
+            var secondRange = Enumerable.Range(secondStart, secondEnd - secondStart + 1);
+            return firstRange.Intersect(secondRange).Any();
+        }
+    }
+}
---
+++ AdventOfCode2022/Day5/ProgramDay5.cs
@@ -0,0 +1,97 @@
+﻿using AdventOfCodeShared;
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode2022.Day5
+{
+    internal class ProgramDay5 : AdventOfCodeProgram
+    {
+        private string[] actions;
+        public ProgramDay5() : base(5)
+        {
+            actions = Array.Empty<string>();
+        }
+
+        protected override string[] Run()
+        {
+            var endResult1 = PerformMoveActions(GetCurrentStackAsDictionary(), actions, false);
+            var endResult2 = PerformMoveActions(GetCurrentStackAsDictionary(), actions, true);
+            var part1 = string.Join("", endResult1.Select(d => d.Value.FirstOrDefault().ToString()));
+            var part2 = string.Join("", endResult2.Select(d => d.Value.FirstOrDefault().ToString()));
+            return new string[] { $"Rearrangement done: {part1}", $"Rearramgement with CrateMover 9001: {part2}" };
+        }
+
+        private Dictionary<int, List<char>> PerformMoveActions(Dictionary<int, List<char>> currentStack, string[] actions, bool canPickupMultipleBoxes)
+        {
+            foreach (var (amount, source, dest) in actions.Select(ParseMoveStringAsActions))
+            {
+                var convertedAmount = Math.Min(amount, currentStack[source].Count());
+                var itemsToMove = currentStack[source].Take(convertedAmount).ToList();
+
+                if (!canPickupMultipleBoxes) itemsToMove.Reverse();
+                currentStack[source].RemoveRange(0, convertedAmount);
+                currentStack[dest].InsertRange(0, itemsToMove);
+
+            }
+            PrintStack(currentStack);
+            return currentStack;
+        }
+
+        private void PrintStack(Dictionary<int, List<char>> copy)
+        {
+            var temp = new Dictionary<int, List<char>>(copy.ToDictionary(c => c.Key, c => new List<char>(c.Value)));
+            var hor = temp.Count();
+            var vert = temp.Select(d => d.Value.Count()).Max();
+            var result = new char[hor][];
+            for (int h = 0; h < hor; h++)
+            {
+                result[h] = new char[vert];
+                var col = temp[h + 1];
+                col.Reverse();
+                for (int v = 0; v < vert; v++)
+                {
+                    var val = col.ElementAtOrDefault(v);
+                    result[h][vert - (v + 1)] = val;
+                }
+                //temp[h + 1].Reverse();
+            }
+
+            for (int v = 0; v < vert; v++)
+            {
+                for (int h = 0; h < hor; h++) Console.Write($"[{result[h][v]}] ");
+                Console.WriteLine();
+            }
+
+            foreach (var p in temp) Console.Write($" {p.Key}  ");
+            Console.WriteLine();
+        }
+
+        private Dictionary<int, List<char>> GetCurrentStackAsDictionary()
+        {
+            var cratesDict = new Dictionary<int, List<char>>();
+            var index = lines.ToList().FindIndex(s => s.StartsWith(" 1"));
+
+            var stackSection = lines.Take(index + 1);
+            var rowWithNumbers = stackSection.Last().Split(" ").Where(s => !string.IsNullOrEmpty(s)).Select(int.Parse);
+            var stacks = stackSection.Take(stackSection.Count() - 1);
+
+            foreach (var n in rowWithNumbers)
+            {
+                var temp = stacks.SelectMany(s => s.Skip((n - 1) * 4).Take(4).Where(c => c != '[' && c != ']' && c != ' '));
+                cratesDict[n] = temp.ToList();
+            }
+            actions = lines.Skip(index + 2).ToArray();
+            return cratesDict;
+        }
+
+        private (int amount, int source, int dest) ParseMoveStringAsActions(string moveString)
+        {
+            Regex re = new(@"\d+");
+            var parsed = re.Matches(moveString).Select(m => int.Parse(m.Value));
+
+            var amount = parsed.ElementAt(0);
+            var source = parsed.ElementAt(1);
+            var dest = parsed.ElementAt(2);
+            return (amount, source, dest);
+        }
+    }
+}
